205295f9ae772867779e3f5f6dfad6a05fdacb31
Mahalakshmi Balachandran <bsmahalakshmi@gmail.com>
2019-04-12 22:41:56 +0530

    moving cross origin to property file

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,9 +7,11 @@ import com.example.util.EmployeeUtil;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.HttpStatus;
@@ -31,13 +33,14 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 @EnableJpaRepositories("com.example*")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"${cross.origin}"})
 @EntityScan("com.example*")
 @ComponentScan("com.example*")
 
 public class DemoApplication {
 
-  @Autowired
+  
+   @Autowired
   private EmployeeService employeeService;
   
    /**
